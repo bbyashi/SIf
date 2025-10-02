85a60345bb806f774f310704ec874b5af2505621
bbyashi <chikubhardwaj1525@gmail.com>
2025-10-02 19:31:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,6 +33,11 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
+API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
+
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/ayanokozii/Sifra2",
@@ -123,3 +128,4 @@ if SUPPORT_CHAT:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
         )
+
